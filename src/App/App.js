2957a81f47a4098d9d1c603a8a33c9cd7ee00f97
Jonathan Gomez G <jg05370@outlook.com>
2019-04-07 16:17:34 -0600

    another commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-import Intro from '../components/Intro';
-import 'fetch'
+import React, { Component } from "react";
+import "./App.css";
+import Intro from "../components/Intro";
+import "fetch";
+import Table from "react-bootstrap/Table";
+
+
 
 class App extends Component {
-  state = 
-  {
-    series : [] 
-  }
-  componentDidMount()
-  {
-    fetch("http://api.tvmaze.com/search/shows?q=thrones").then(res => res.json())
-    .then(json => this.setState({series: json}));
+  state = {
+    series: []
+  };
+  componentDidMount() {
+    fetch("http://api.tvmaze.com/search/shows?q=thrones")
+      .then(res => res.json())
+      .then(json => this.setState({ series: json }));
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <h1 className="App-title">TV Series List</h1>
+          <h1 className="App-title">TV Series List</h1>
         </header>
-        <Intro message="Here is where you can find all your beloved series"/>
-        The length of the series array = {this.state.series.length}
+        <Intro message="Here is where you can find all your beloved series" />
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Premiered</th>
+              <th>Language</th>
+              <th>Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.series.map((element, ElementIndex) => {
+            return(
+            <tr key={ElementIndex}>
+              <td>{element.show.name}</td>
+              <td>{element.show.status}</td>
+              <td>{element.show.premiered}</td>
+              <td>{element.show.language}</td>
+              <td dangerouslySetInnerHTML={{__html: element.show.summary}}></td>
+              
+            </tr>);
+          })}
+          </tbody>
+        </Table>
+        ; The length of the series array = {this.state.series.length}
       </div>
     );
   }
